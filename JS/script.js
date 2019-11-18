5191bdec0074ab159b1a01c73a150526ad6810f3
DOJ745 <andreyfa12@mail.ru>
2019-11-18 23:48:09 +0300

    Подготовка к парсингу

--- JS/script.js
+++ JS/script.js
@@ -24,11 +24,24 @@ else
     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
 
-    xmlhttp.open("GET","", false);
-    xmlhttp.overrideMimeType('application/xml');
-    xmlhttp.send();
-    xmlDoc = xmlhttp.responseText;
+xmlhttp.open("GET","https://raw.githubusercontent.com/DOJ745/DOJ745.github.io/master/XML/Database.xml", false);
+xmlhttp.overrideMimeType('application/xml');
+xmlhttp.send();
+xmlDoc = xmlhttp.responseText;
 
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(xmlDoc, "application/xml");
-    var lineItems = doc.getElementsByTagName("About");
+var parser = new DOMParser();
+var doc = parser.parseFromString(xmlDoc, "application/xml");
+
+/* Получил массив тегов About
+У меня пока что только 1 тег about*/
+
+var Site = doc.getElementsByTagName("About");
+
+/*Получаю содержимое тега text*/
+
+var first_parse = aboutText[0].getElementsByTagName("text")[0].innerHTML;
+
+/* Получаю абзац, куда буду вставлять текст*/
+//var p_parse = document.getElementById("parse_here").innerHTML;
+
+document.getElementById("parse_here") = first_parse;
