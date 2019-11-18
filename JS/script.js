f7a4bed51a4dbd1adc2ea84d1e0004af1aae9800
DOJ745 <andreyfa12@mail.ru>
2019-11-18 23:36:36 +0300

    Подготовка к парсингу текста в about_index.html

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,24 @@ menu.onclick = function CLK()
         change_class.className = "topnav";
     }
 }
+
+
+/* Парсер XML*/
+
+if (window.XMLHttpRequest)
+{ // для IE7+, Firefox, Chrome, Opera, Safari
+         xmlhttp = new XMLHttpRequest();
+}
+else
+{ // для IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+    xmlhttp.open("GET","", false);
+    xmlhttp.overrideMimeType('application/xml');
+    xmlhttp.send();
+    xmlDoc = xmlhttp.responseText;
+
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(xmlDoc, "application/xml");
+    var lineItems = doc.getElementsByTagName("About");
