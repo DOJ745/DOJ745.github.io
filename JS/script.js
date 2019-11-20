73344c70ebad06b298529c41cc5300a31f6490ed
DOJ745 <andreyfa12@mail.ru>
2019-11-21 00:08:24 +0300

    Парсим теги из XML в Longsword.html

--- JS/script.js
+++ JS/script.js
@@ -49,3 +49,16 @@ var third_parse = aboutText[0].getElementsByTagName("text")[2].innerHTML;
 document.getElementById("parse_here").innerHTML = first_parse;
 document.getElementById("parse_here2").innerHTML = second_parse;
 document.getElementById("parse_here3").innerHTML = third_parse;
+
+
+        /* Попытка вставить атрибуты из XML на страницу Longsword*/
+
+var longswordInfo = doc.getElementsByTagName("Longsword");
+var longsword_pic1 = longswordInfo[0].getElementsByTagName("Picture1")[0];
+
+var longsword_pic1_attrPath = longsword_pic1.getAttribute("path");
+var longsword_pic1_attrBorder = longsword_pic1.getAttribute("border");
+
+var img_longsword = document.getElementsByTagName("img");
+img_longsword[1].setAttribute("src", longsword_pic1_attrPath);
+img_longsword[1].setAttribute("class", longsword_pic1_attrBorder);
