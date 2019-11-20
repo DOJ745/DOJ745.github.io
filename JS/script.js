f539dccc6da0f0faaf3a555651a79ad85c453b4a
DOJ745 <andreyfa12@mail.ru>
2019-11-20 12:35:00 +0300

    Смотрю поведение парсера

--- JS/script.js
+++ JS/script.js
@@ -49,21 +49,3 @@ var third_parse = aboutText[0].getElementsByTagName("text")[2].innerHTML;
 document.getElementById("parse_here").innerHTML = first_parse;
 document.getElementById("parse_here2").innerHTML = second_parse;
 document.getElementById("parse_here3").innerHTML = third_parse;
-
-
-
-/* Парсим информацию об картинках на страницу longsword_index.html */
-/*var firstPicture = longswordPictures[0].getElementsByTagName("Picture1");*/
-
-/*
-var longswordInfo = doc.getElementsByTagName("Longsword");
-var longswordPicture = longswordInfo[0].getElementsByTagName("Picture1");
-
-
-
-var firstAttr = longswordPicture[0].getAttribute("path");
-var secondAttr = longswordPicture[0].getAttribute("border");
-
-var imgArray = document.getElementsByTagName("img");
-
-imgArray[0].setAttribute(src, firstAttr); */
