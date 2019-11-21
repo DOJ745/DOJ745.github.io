da53a4900fae5ec4c38fa7671e8b07e8626e4a7d
DOJ745 <andreyfa12@mail.ru>
2019-11-22 00:24:22 +0300

    Мелочные изменения для парсинга

--- JS/longswordParse.js
+++ JS/longswordParse.js
@@ -19,9 +19,9 @@ var longswordInfo = doc.getElementsByTagName("Longsword");
 var picture1 = longswordInfo[0].getElementsByTagName("Picture1");
 var picture2 = longswordInfo[0].getElementsByTagName("Picture2");
 
-var path_attr1 = picture1.getAttribute("path");
-var class_attr = picture1.getAttribute("border");
-var path_attr2 = picture2.getAttribute("path");
+var path_attr1 = picture1[0].getAttribute("path");
+var class_attr = picture1[0].getAttribute("border");
+var path_attr2 = picture2[0].getAttribute("path");
 
 var img_arr = document.getElementsByTagName("img");
 
@@ -30,15 +30,3 @@ img_arr[1].setAttribute("class", class_attr);
 
 img_arr[2].setAttribute("src", path_attr2);
 img_arr[2].setAttribute("class", class_attr);
-
-/* Попытка вставить атрибуты из XML на страницу Longsword*/
-
-/*var longswordPics = doc.getElementsByTagName("Longsword");
-var longsword_pic1 = longswordPics[0].getElementsByTagName("Picture1")[0];
-
-var longsword_pic1_attrPath = longsword_pic1.getAttribute("path");
-var longsword_pic1_attrBorder = longsword_pic1.getAttribute("border");
-
-var img_longsword = document.getElementsByTagName("img");
-img_longsword[1].setAttribute("src", longsword_pic1_attrPath);
-img_longsword[1].setAttribute("class", longsword_pic1_attrBorder);*/
