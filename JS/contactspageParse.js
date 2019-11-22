bd2e1d34dcbc43c18a35235af5f5a9e074d43d04
DOJ745 <andreyfa12@mail.ru>
2019-11-22 10:10:15 +0300

    Новая страница и парсер к ней

---
+++ JS/contactspageParse.js
@@ -0,0 +1,22 @@
+if (window.XMLHttpRequest)
+{ // для IE7+, Firefox, Chrome, Opera, Safari
+         xmlhttp = new XMLHttpRequest();
+}
+else
+{ // для IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+xmlhttp.open("GET","https://raw.githubusercontent.com/DOJ745/DOJ745.github.io/master/XML/Database.xml", false);
+xmlhttp.overrideMimeType('application/xml');
+xmlhttp.send();
+xmlDoc = xmlhttp.responseText;
+
+var parser = new DOMParser();
+var doc = parser.parseFromString(xmlDoc, "application/xml");
+
+var contactInfo = doc.getElementsByTagName("Contact");
+var contact_text = contactInfo[0].getElementsByTagName("text");
+
+var p_parse = document.getElementsByTagNameName("p");
+p_parse[0].innerHTML = contact_text[0].innerHTML;
