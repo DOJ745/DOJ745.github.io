8ec8bcb14698e8b25ae86046863deccd20a4ba40
DOJ745 <andreyfa12@mail.ru>
2019-11-28 22:22:21 +0300

    Изменение ссылок для мобильных устройств, теги alt и комменты
    Теги alt лучше заполнить вручную для всех картинок, ибо их парсинг из xml такая себе идея. Сделал себе поясняющие комментарии, и сделал так, чтобы на мобилках я не переходил на главные страницы "Оружие" и "Доспехи". Получилось что-то вроде отдельной версии для мобилок.

--- JS/mobileAdapt.js
+++ JS/mobileAdapt.js
@@ -4,9 +4,19 @@ menu.onclick = function CLK()
 {
     var change_class = document.getElementById("myTopnav");
     
+    var a_weaponMain = document.getElementById("Weapon");
+    var a_armorMain = document.getElementById("Armor");
+    
     if(change_class.className === "topnav")
     {
         change_class.className += " responsive";
+        /* Меняю  ссылки для того, чтобы на мобильной версии люди
+        при нажатии на "Доспехи" или "Оружие" не заходили на их
+        главные страницы, а чтобы они переходили на уже отсортированные
+        наборы оружий или доспехов.
+        */
+        a_weaponMain.setAttribute("href", "#");
+        a_armorMain.setAttribute("href", "#");
     }
     else
     {   
