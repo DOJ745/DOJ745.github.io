2c49c33d631caa7897a34900f56953fb225d24d9
DOJ745 <andreyfa12@mail.ru>
2019-12-05 11:18:58 +0300

    Пробуем запарсить табдицу по старинке

--- JS/footerParse.js
+++ JS/footerParse.js
@@ -26,10 +26,10 @@ for(let i = 0; i < footer_parse_p.length; i++)
     footer_parse_p[i].innerHTML = footer_text[i].innerHTML;    
 }
 
-var footer_table = footerInfo[0].getElementsByTagName("tableinfo");
-var footer_parse_table = document.getElementById("parseTable");
+/*var footer_table = footerInfo[0].getElementsByTagName("tableinfo");
+var footer_parse_table = document.getElementById("parseTable");*/
 
-footer_parse_table.innerHTML = footer_table.innerHTML;
+/*footer_parse_table.innerHTML = footer_table.innerHTML;*/
 
 /*for(let i = 0, let j = 0; i < footer_td.length; i++)
 {
@@ -44,3 +44,13 @@ footer_parse_table.innerHTML = footer_table.innerHTML;
     }
     footer_td[i].innerHTML = footer_t[i].innerHTML;
 }*/
+
+var table_parse = "<table border = '1px'>";
+var table_parse += "<tr><td>ФИО</td><td>Фурсик Андрей Алексеевич</td></tr>";
+var table_parse += "<tr><td>Факультет</td><td>ИТ</td></tr>";
+var table_parse += "<tr><td>Специальность</td><td>ПОИБМС</td></tr>";
+var table_parse += "<tr><td>Группа</td><td>7</td></tr>";
+var table_parse += "<tr><td>Год</td><td>2019</td></tr></table>";
+
+var footer_parse_table = document.getElementById("parseTable");
+footer_parse_table.innerHTML = table_parse.innerHTML;
