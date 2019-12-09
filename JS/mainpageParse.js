7aefe50d64d719affeaade3c275eec223a7911c0
DOJ745 <andreyfa12@mail.ru>
2019-12-09 23:41:51 +0300

    Парсинг для главной страницы

--- JS/mainpageParse.js
+++ JS/mainpageParse.js
@@ -14,3 +14,33 @@ xmlDoc = xmlhttp.responseText;
 
 var parser = new DOMParser();
 var doc = parser.parseFromString(xmlDoc, "application/xml");
+
+var mainpageInfo = doc.getElementsByTagName("Mainpage");
+
+var text_parse = mainpageInfo[0].getElementsByTagName("text");
+var p_locale = document.getElementById("picParse");
+var p_parse = p_locale.getElementsByTagName("p");
+
+for(let i = 0; i < p_parse.length; i++)
+{
+    p_parse[i].innerHTML = text_parse[i].innerHTML;
+}
+
+var pic_parse = mainpageInfo[0].getElementsByTagName("Picture");
+
+var pic_path = new Array();
+for(let i = 0; i < pic_parse.length; i++)
+{
+    pic_path.push(pic_parse[i].getAttribute("path"));
+}
+
+var border_attr = pic_parse[0].getAttribute("border");
+
+var pic_locale = document.getElementById("picParse");
+var img_arr = pic_locale.getElementsByTagName("img");
+
+for(let i = 0; i < picList.length; i++)
+{
+    img_arr[i].setAttribute("src", pic_path[i]);
+    img_arr[i].setAttribute("class", border_attr);
+}
