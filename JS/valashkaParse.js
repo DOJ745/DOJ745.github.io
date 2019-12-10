ffc6c0352bd7462f83241442062a54958eca898f
DOJ745 <andreyfa12@mail.ru>
2019-12-10 20:26:31 +0300

    Подготовка к добавлению топоров

---
+++ JS/valashkaParse.js
@@ -0,0 +1,42 @@
+if (window.XMLHttpRequest)
+{ // для IE7+, Firefox, Chrome, Opera, Safari
+         xmlhttp = new XMLHttpRequest();
+}
+else
+{ // для IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+xmlhttp.open("GET","https://raw.githubusercontent.com/DOJ745/DOJ745.github.io/master/XML/Database.xml", false);
+xmlhttp.overrideMimeType('application/xml');
+xmlhttp.send();
+xmlDoc = xmlhttp.responseText;
+
+var parser = new DOMParser();
+var doc = parser.parseFromString(xmlDoc, "application/xml");
+        
+var axeInfo = doc.getElementsByTagName("Valashka");
+var pictures = axeInfo[0].getElementsByTagName("Picture");
+var text_parse = axeInfo[0].getElementsByTagName("text");
+
+var p_arr = document.getElementsByTagName("p");
+for(let i = 0; i < text_parse.length; i++)
+{
+    p_arr[i].innerHTML = text_parse[i].innerHTML;
+}
+
+var border_attr = pictures[0].getAttribute("border");
+var path_attrs = new Array();
+
+for(let i = 0; i < pictures.length; i++)
+{
+    path_attrs.push(pictures[i].getAttribute("path"));
+}
+    
+var img_parse = document.getElementById("picParse");
+var img_arr = img_parse.getElementsByTagName("img");
+for(let i = 0; i < img_arr.length; i++)
+{
+    img_arr[i].setAttribute("src", path_attrs[i]);
+    img_arr[i].setAttribute("class", border_attr);
+}
