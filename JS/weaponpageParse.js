55d9787030805b873bc6ced7fe09c1f3f76f5f55
DOJ745 <andreyfa12@mail.ru>
2019-12-11 12:08:15 +0300

    Парсим контент на главную страницу об оружии

--- JS/weaponpageParse.js
+++ JS/weaponpageParse.js
@@ -14,3 +14,22 @@ xmlDoc = xmlhttp.responseText;
 
 var parser = new DOMParser();
 var doc = parser.parseFromString(xmlDoc, "application/xml");
+
+var all_weaponInfo = doc.getElementsByTagName("Parsing");
+var pictures = all_weaponInfo[0].getElementsByTagName("Picture");
+
+var border_attr = pictures[0].getAttribute("border");
+var path_attrs = new Array();
+
+for(let i = 0; i < pictures.length; i++)
+{
+    path_attrs.push(pictures[i].getAttribute("path"));
+}
+    
+var img_parse = document.getElementById("picParse");
+var img_arr = img_parse.getElementsByTagName("img");
+for(let i = 0; i < img_arr.length; i++)
+{
+    img_arr[i].setAttribute("src", path_attrs[i]);
+    img_arr[i].setAttribute("class", border_attr);
+}
